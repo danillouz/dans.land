d1f323ea25bb5e31030abb4fddd104f36c451873
Daniël Illouz <danillouz@users.noreply.github.com>
2024-11-10 17:45:53 +0100

    Add homepage

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,8 @@
 /** @type {import("prettier").Config} */
 module.exports = {
   semi: false,
+
+  // To e.g. prevent spacing issues when adding anchor
+  // tags paragraphs.
+  htmlWhitespaceSensitivity: "ignore",
 }
