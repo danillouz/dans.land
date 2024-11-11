1ae4ae2ad112c0c4420e073dc5c0dd4be3ec93ba
Daniël Illouz <danillouz@users.noreply.github.com>
2024-11-11 20:39:52 +0100

    Remove prettier htmlWhitespaceSensitivity
    Apparently _having_ this set to ignore messes with the whitespace?
    I don't get it TBH, but this seems to work now.

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,4 @@
 /** @type {import("prettier").Config} */
 module.exports = {
   semi: false,
-
-  // To e.g. prevent spacing issues when adding anchor
-  // tags paragraphs.
-  htmlWhitespaceSensitivity: "ignore",
 }
