916b682a3d856249b48256fc96b17515e874d5cb
Daniël Illouz <6201287+danillouz@users.noreply.github.com>
2026-07-26 16:08:09 +0200

    Add about page

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { fileURLToPath, URL } from "node:url"
+
+import { defineConfig } from "vite"
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        home: fileURLToPath(new URL("./index.html", import.meta.url)),
+        about: fileURLToPath(new URL("./about/index.html", import.meta.url)),
+      },
+    },
+  },
+})
